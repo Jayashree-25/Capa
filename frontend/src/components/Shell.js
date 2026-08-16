8e30816abc8f566afda3428d0251b55ce92cdab4
jayashre-25 <dasjayashree8260@gmail.com>
2026-08-16 18:05:05 +0530

    Add Shell component for layout structure with header and sidebar

---
+++ frontend/src/components/Shell.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Header from './Header';
+import Sidebar from './Sidebar';
+
+const Shell = ({ user, onLogout, children }) => {
+  const [navOpen, setNavOpen] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-slate-50">
+      <Header user={user} onLogout={onLogout} onMenuClick={() => setNavOpen(true)} />
+      <div className="flex">
+        <aside className="hidden md:block w-56 shrink-0 bg-white border-r border-gray-200 sticky top-14 self-start">
+          <Sidebar user={user} />
+        </aside>
+        {navOpen && (
+          <div className="fixed inset-0 z-40 md:hidden">
+            <div className="absolute inset-0 bg-black/30" onClick={() => setNavOpen(false)} />
+            <aside className="absolute left-0 top-0 bottom-0 w-64 bg-white shadow-lg">
+              <Sidebar user={user} onNavigate={() => setNavOpen(false)} />
+            </aside>
+          </div>
+        )}
+        <main className="flex-1 min-w-0 p-4 sm:p-6">{children}</main>
+      </div>
+    </div>
+  );
+};
+
+export default Shell;
