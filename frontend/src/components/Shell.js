51382f7759b6d39354d56583b304137b001bcb54
jayashre-25 <dasjayashree8260@gmail.com>
2026-08-19 22:14:30 +0530

    Enhance Shell component: Add logout functionality to Sidebar in both desktop and mobile views

--- frontend/src/components/Shell.js
+++ frontend/src/components/Shell.js
@@ -9,14 +9,14 @@ const Shell = ({ user, onLogout, children }) => {
     <div className="min-h-screen bg-slate-50">
       <Header user={user} onLogout={onLogout} onMenuClick={() => setNavOpen(true)} />
       <div className="flex">
-        <aside className="hidden md:block w-56 shrink-0 bg-white border-r border-gray-200 sticky top-14 self-start">
-          <Sidebar user={user} />
+        <aside className="hidden md:block w-56 shrink-0 bg-white border-r border-gray-200 sticky top-14 self-start h-[calc(100vh-3.5rem)]">
+          <Sidebar user={user} onLogout={onLogout} />
         </aside>
         {navOpen && (
           <div className="fixed inset-0 z-40 md:hidden">
             <div className="absolute inset-0 bg-black/30" onClick={() => setNavOpen(false)} />
             <aside className="absolute left-0 top-0 bottom-0 w-64 bg-white shadow-lg">
-              <Sidebar user={user} onNavigate={() => setNavOpen(false)} />
+              <Sidebar user={user} onLogout={onLogout} onNavigate={() => setNavOpen(false)} />
             </aside>
           </div>
         )}
